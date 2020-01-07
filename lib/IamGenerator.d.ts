8becd08677ae3eeb1366ba81dd84211be6f4a7fe
srihap <srihap@amazon.com>
2020-01-07 17:16:25 -0500

    updates

--- lib/IamGenerator.d.ts
+++
@@ -1,8 +0,0 @@
-import { Construct, StackProps } from '@aws-cdk/core';
-export interface IamGeneratorProps extends StackProps {
-    readonly configPath: string;
-    readonly policyPath: string;
-}
-export declare class IamGenerator extends Construct {
-    constructor(scope: Construct, id: string, props: IamGeneratorProps);
-}
